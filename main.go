8501c3006f1819734bd1fff73721ed6546dbce4d
Joe Oliver <joey_oliver@hotmail.com>
2018-10-19 14:09:37 +0100

    Sniff the content type from the file

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/joho/godotenv"
 	"github.com/minio/minio-go"
 	"github.com/rs/xid"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"strings"
 )
 
+type Upload struct {
+	Url string
+}
 
 var endpoint string = "ams3.digitaloceanspaces.com"
 var spaceName string = "learnabout-dev" // Space names must be globally unique
@@ -48,42 +52,8 @@ func main(){
 
 	h := http.NewServeMux()
 
-	authenticate := func(r *http.Request) (bool, error) {
-		authorization := r.Header.Get("Authorization")
-		if authorization == "" {
-			return false, nil
-		}
-
-		authorization = strings.Replace(authorization, "Bearer ", "", 1)
-
-		parser := jwt.Parser{
-			SkipClaimsValidation: true,
-		}
-
-		token, err := parser.Parse(authorization, func(t *jwt.Token) (interface{}, error){
-
-			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
-			}
-
-			return appSecret, nil
-		})
-
-		if err != nil {
-			fmt.Printf("error in parsing token: %v", err)
-		}
-
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			fmt.Println(claims["userId"])
-			return true, nil
-		} else {
-			fmt.Println(err)
-			return false, nil
-		}
-	}
-
 	h.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
-		authenticated, err := authenticate(r)
+		authenticated, err := authenticate(r, appSecret)
 
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -97,53 +67,124 @@ func main(){
 			return
 		}
 
-		// this is implicitly setting the status header to 200
-		//fmt.Fprintf(w, "Hello %s", r.Header.Get("Content-Type"))
-
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintf(w, "Not a post request")
 			return
-		} else {
-			file, header, err := r.FormFile("data")
-			if file == nil {
-				w.WriteHeader(http.StatusBadRequest)
-				fmt.Fprintf(w, "Error accessing the file")
-				return
-			}
-
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				fmt.Fprintf(w, "Error accessing the file")
-				log.Printf("%v", err)
-				return
-			}
-
-			// Cors stuff
-			r.Header.Set("Access-Control-Allow-Origin", "http://localhost:8080")
-
-			// Generate ID for the file
-			id := xid.New()
-
-			n, err := client.PutObject(spaceName, id.String(), file, header.Size, minio.PutObjectOptions{})
-
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				fmt.Fprintf(w, "Error uploading the file")
-			}
-
-			fmt.Fprintf(w, "File %s uploaded successfully. %d bytes", id, n)
 		}
+
+		file, header, err := r.FormFile("data")
+
+		if err != nil || file == nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Error accessing the file")
+			return
+		}
+
+		defer file.Close()
+
+		contentType, err := getFileContentType(file)
+		content := strings.Split(contentType, "/")
+		contentBase, contentSuffix := content[0], content[1]
+
+		if err != nil || contentBase != "image" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "No Image Found")
+			log.Printf("%v", err)
+			return
+		}
+
+		// Cors stuff
+		r.Header.Set("Access-Control-Allow-Origin", "http://localhost:8080")
+
+		// Generate ID for the file
+		id := xid.New()
+
+		// Set extra content-policy keys
+		userMetaData := map[string]string{"x-amz-acl": "public-read"}
+		_, err = client.PutObject(spaceName, id.String() + "." + contentSuffix, file, header.Size, minio.PutObjectOptions{
+			ContentType: contentType,
+			UserMetadata: userMetaData,
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Error uploading the file")
+		}
+
+		w.WriteHeader(http.StatusOK)
+
+		urlComponents := []string {
+			endpoint,
+			spaceName,
+			id.String() + "." + contentSuffix,
+		}
+
+		upload := Upload {
+			Url: "http://" + strings.Join(urlComponents, "/"),
+		}
+
+		body, err := json.Marshal(&upload)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "Internal Server Error")
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(body)
 	})
 
 	serverErr := http.ListenAndServe(":8081", h)
 	log.Fatal(serverErr)
 }
 
-
-func getUserId(ctx context.Context, secret string){
-	authorization := ctx.Value("Authorization")
-	if authorization != nil {
-		fmt.Println("authorization", authorization)
+func authenticate(r *http.Request, appSecret []byte) (bool, error) {
+	authorization := r.Header.Get("Authorization")
+	if authorization == "" {
+		return false, nil
 	}
+
+	authorization = strings.Replace(authorization, "Bearer ", "", 1)
+
+	parser := jwt.Parser{
+		SkipClaimsValidation: true,
+	}
+
+	token, err := parser.Parse(authorization, func(t *jwt.Token) (interface{}, error){
+
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return appSecret, nil
+	})
+
+	if err != nil {
+		fmt.Printf("error in parsing token: %v", err)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["userId"])
+		return true, nil
+	} else {
+		fmt.Println(err)
+		return false, nil
+	}
+}
+
+// Brazenly Stolen from here: https://golangcode.com/get-the-content-type-of-file/
+func getFileContentType(file multipart.File) (string, error) {
+	// Only the first 512 bytes are used to sniff the content type.
+	buffer := make([]byte, 512)
+	_, err := file.Read(buffer)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Use the net/http package's handy DectectContentType function. Always returns a valid
+	// content-type by returning "application/octet-stream" if no others seemed to match.
+	contentType := http.DetectContentType(buffer)
+
+	return contentType, nil
 }
