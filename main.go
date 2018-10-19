4530079645f894779fe9d6e8539c66f095305418
Joe Oliver <joey_oliver@hotmail.com>
2018-10-19 13:25:42 +0100

    JWT Authentication

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/joho/godotenv"
 	"github.com/minio/minio-go"
 	"github.com/rs/xid"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 
@@ -28,6 +31,7 @@ func main(){
 	accessKey := os.Getenv("SPACES_KEY")
 	secKey := os.Getenv("SPACES_SECRET")
 	ssl := true
+	appSecret := []byte(os.Getenv("APP_SECRET"))
 
 	//Initiate a client using DigitalOcean Spaces.
 	client, err := minio.New(endpoint, accessKey, secKey, ssl)
@@ -36,9 +40,63 @@ func main(){
 		log.Fatal(err)
 	}
 
+	// Create a client for our authentication service
+	//db := prisma.New(&prisma.PrismaOptions{
+	//	Endpoint: "http://localhost:4466/",
+	//})
+	//ctx := context.Background()
+
 	h := http.NewServeMux()
 
+	authenticate := func(r *http.Request) (bool, error) {
+		authorization := r.Header.Get("Authorization")
+		if authorization == "" {
+			return false, nil
+		}
+
+		authorization = strings.Replace(authorization, "Bearer ", "", 1)
+
+		parser := jwt.Parser{
+			SkipClaimsValidation: true,
+		}
+
+		token, err := parser.Parse(authorization, func(t *jwt.Token) (interface{}, error){
+
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
+			}
+
+			return appSecret, nil
+		})
+
+		if err != nil {
+			fmt.Printf("error in parsing token: %v", err)
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			fmt.Println(claims["userId"])
+			return true, nil
+		} else {
+			fmt.Println(err)
+			return false, nil
+		}
+	}
+
 	h.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		authenticated, err := authenticate(r)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "Internal Server Error")
+			return
+		}
+
+		if authenticated == false {
+			w.WriteHeader(http.StatusForbidden)
+			fmt.Fprint(w, "Not Authorized")
+			return
+		}
+
 		// this is implicitly setting the status header to 200
 		//fmt.Fprintf(w, "Hello %s", r.Header.Get("Content-Type"))
 
@@ -80,4 +138,12 @@ func main(){
 
 	serverErr := http.ListenAndServe(":8081", h)
 	log.Fatal(serverErr)
+}
+
+
+func getUserId(ctx context.Context, secret string){
+	authorization := ctx.Value("Authorization")
+	if authorization != nil {
+		fmt.Println("authorization", authorization)
+	}
 }
